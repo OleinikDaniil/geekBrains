2e5e6e4a926ec002f6d50a03beabdfc2360f0bbb
Daniil <naetlass@gmail.com>
2020-12-11 09:07:55 +0300

    Created HelloWorld file for the Lesson 1

---
+++ Introduction to C# Lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string nameEntered = Console.ReadLine();
+            Console.WriteLine($"Привет, {nameEntered}, сегодня {DateTime.Now}");
+        }
+    }
+}
