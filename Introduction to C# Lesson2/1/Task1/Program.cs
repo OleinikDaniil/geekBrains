abffd32e6be9a445bae040a3afe997ba842de224
Daniil <naetlass@gmail.com>
2020-12-14 19:19:12 +0300

    First task completed

---
+++ Introduction to C# Lesson2/1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+            Console.WriteLine("Введите минимальную дневную температуру");
+            string minTemp = Console.ReadLine();
+            int minTempInt = Convert.ToInt32(minTemp);
+            Console.WriteLine("Введите максимальную дневную температуру");
+            string maxTemp = Console.ReadLine();
+            int maxTempInt = Convert.ToInt32(maxTemp);
+            int medianTempInt = (maxTempInt + minTempInt) / 2 ;
+            Console.WriteLine( "Средняя температура за день равна: " + medianTempInt + " градусов.");
+        }
+    }
+}
